b9c73789eafc6b0ebf8c0e21044f76fb97ff27ce
Kannapiran-Siddarth-216469850 <siddarth@my.yorku.ca>
2020-01-18 00:29:50 -0500

    Lab2 completed

--- Lab2/Dio.sh
+++ Lab2/Dio.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-echo This is Lab2 Q2 file
+x=$1
+y=$2
+z=$3
+w=$4
+
+for ((i=1; i<=$x; i++))
+do 
+    for ((j=1; j<=$y; j++))
+    do
+        for ((k=1; k<=$z; k++))
+        do
+            for ((l=1; l<=$w; l++))
+            do       
+                if [ $(($i * $i * $i + $j * $j * $j)) = $(($k * $k * $k + $l * $l * $l)) ]
+                then
+                echo x=$i, y=$j, z=$k, w=$l
+                else
+                echo No solution
+                fi                               
+            done
+        done        
+    done
+done
